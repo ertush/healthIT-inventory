be8ff24ec93b266efd4ab3227da9a064ddccaab7
erotush <erotush77@gmail.com>
2022-03-16 13:15:54 +0300

    Add: Authentication middleware

--- App/cmd/main.go
+++ App/cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/ertush/healthIT-inventory/App/pkg/db"
-	"github.com/ertush/healthIT-inventory/App/pkg/middleware"
 	"github.com/ertush/healthIT-inventory/App/pkg/handlers"
 	"github.com/gorilla/mux"
 )
@@ -15,11 +14,11 @@ func main() {
 	h := handlers.New(DB)
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/books", Middleware(http.HandleFunc(h.GetAllBooks)).Methods(http.MethodGet)
-	router.HandleFunc("/api/books/{id}", Middleware(http.HandleFunc(h.GetBook)).Methods(http.MethodGet)
-	router.HandleFunc("/api/books", Middleware(http.HandleFunc(h.AddBook)).Methods(http.MethodPost)
-	router.HandleFunc("/api/books/{id}", Middleware(http.HandleFunc(h.UpdateBook)).Methods(http.MethodPut)
-	router.HandleFunc("/api/books/{id}", Middleware(http.HandleFunc(h.DeleteBook)).Methods(http.MethodDelete)
+	router.HandleFunc("/api/books", Middleware(http.HandleFunc(h.GetAllBooks))).Methods(http.MethodGet)
+	router.HandleFunc("/api/books/{id}", Middleware(http.HandleFunc(h.GetBook))).Methods(http.MethodGet)
+	router.HandleFunc("/api/books", Middleware(http.HandleFunc(h.AddBook))).Methods(http.MethodPost)
+	router.HandleFunc("/api/books/{id}", Middleware(http.HandleFunc(h.UpdateBook))).Methods(http.MethodPut)
+	router.HandleFunc("/api/books/{id}", Middleware(http.HandleFunc(h.DeleteBook))).Methods(http.MethodDelete)
 
 	// auth endpoints
 	router.HandleFunc("/api/register", h.Register).Methods(http.MethodPost)
